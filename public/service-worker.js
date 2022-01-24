ac304c625d8c04c3543db3225e0db91d604bc4a7
Brett Howard <howardcompositions@gmail.com>
2022-01-23 20:12:59 -0700

    finalizing

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,22 @@
 const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE_NAME = 'data-cache-v2';
 
 const FILES_TO_CACHE = [
-    './index.html',
-    './css/styles.css',
-    './js/index.js',
-    './js/idb.js',
-    './manifest.json',
-    './icons/icon-72x72.png',
-    './icons/icon-96x96.png',
-    './icons/icon-128x128.png',
-    './icons/icon-144x144.png',
-    './icons/icon-152x152.png',
-    './icons/icon-192x192.png',
-    './icons/icon-384x384.png',
-    './icons/icon-512x512.png'
+    '/index.html',
+    '/css/styles.css',
+    '/js/index.js',
+    '/js/idb.js',
+    '/manifest.json',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', function (e) {
@@ -45,18 +46,42 @@ self.addEventListener('activate', function (e) {
     )
 });
 
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) { // if cache is available, respond with cache
-                console.log('responding with cache : ' + e.request.url);
-                return request
-            } else {       // if there are no cache, try fetching request
-                console.log('file is not cached, fetching : ' + e.request.url);
-                return fetch(e.request)
-            }
-
-        })
-    )
-});
+self.addEventListener('fetch', function(evt) {
+    if (evt.request.url.includes('/api/')) {
+      evt.respondWith(
+        caches
+          .open(DATA_CACHE_NAME)
+          .then(cache => {
+            return fetch(evt.request)
+              .then(response => {
+                // If the response was good, clone it and store it in the cache.
+                if (response.status === 200) {
+                  cache.put(evt.request.url, response.clone());
+                }
+  
+                return response;
+              })
+              .catch(err => {
+                // Network request failed, try to get it from the cache.
+                return cache.match(evt.request);
+              });
+          })
+          .catch(err => console.log(err))
+      );
+  
+      return;
+    }
+  
+    evt.respondWith(
+      fetch(evt.request).catch(function() {
+        return caches.match(evt.request).then(function(response) {
+          if (response) {
+            return response;
+          } else if (evt.request.headers.get('accept').includes('text/html')) {
+            // return the cached home page for all requests for html pages
+            return caches.match('/');
+          }
+        });
+      })
+    );
+  });
